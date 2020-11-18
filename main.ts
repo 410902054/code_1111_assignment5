9ceab21f65d3799b694b96d4b212359e7e348913
410902054 <71751283+410902054@users.noreply.github.com>
2020-11-18 15:04:10 +0800

    T2

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
 basic.forever(function () {
     for (let j = 0; j <= 4; j++) {
         for (let i = 0; i <= j; i++) {
-            if (0 + i == 0 + j) {
-                led.plot(2 + i, 2)
-                led.plot(2 - i, 2)
-                led.plot(2, 2 + i)
-                led.plot(2, 2 - i)
-            }
+            led.plot(2 + i, 2)
+            led.plot(2 - i, 2)
+            led.plot(2, 2 + i)
+            led.plot(2, 2 - i)
         }
-        basic.pause(100)
+        basic.pause(500)
     }
     basic.clearScreen()
 })
